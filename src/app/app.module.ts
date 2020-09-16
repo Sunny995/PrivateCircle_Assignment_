b5b9baf59bbdcf97de2739117cc169704545f2f7
Sunny Kumar <sunny.kumar@stradus.com>
2020-09-16 17:04:32 +0530

    First commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { DetailsComponent } from './components/details/details.component';
+import { OptionsComponent } from './components/options/options.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    DashboardComponent,
+    DetailsComponent,
+    OptionsComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/components/dashboard/dashboard.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { List } from 'src/app/interfaces/list';
+import data from "../../data.json";
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss']
+})
+export class DashboardComponent implements OnInit {
+  renderedList: List[] = [];
+  completeListData: List[] = [];
+  details = [];
+  selectedItemName= '' ;
+
+  constructor() { }
+
+  ngOnInit(): void {
+    this.completeListData = Object.assign([], data);;
+    this.renderedList = Object.assign([], this.completeListData);
+
+  }
+  
+  showDetails(selectedItemDetails: any,selectedItemName: any): void {
+    this.details = Object.assign([], selectedItemDetails);
+    this.selectedItemName = selectedItemName;
+  }
+
+  searchTable(value: string): void {
+    var searchTerms = value.trim();
+    var filteredItems = [];
+    if (searchTerms === '') {
+      this.renderedList = Object.assign([], this.completeListData);
+    } else {
+
+      filteredItems = Object.assign([], this.completeListData).filter(
+        item => item.name.toLowerCase().indexOf(value.toLowerCase()) > -1
+      )
+      this.renderedList = Object.assign([], filteredItems);
+
+    }
+  }
+
+
+}
